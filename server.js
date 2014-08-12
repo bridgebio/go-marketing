01d3aedb35b878a23b0c16b33a76e750b0efd3a4
Steven Hernandez <shernandez@glgroup.com>
2014-08-12 17:12:58 -0500

    Updated request.js and request.html with browser-request and lodash for micro-templating. Input form fields needed -webkit-font-smoothing:antialiased for better font smoothing and readability.

--- server.js
+++ server.js
@@ -31,11 +31,8 @@ app.get('/download', function(req, res, next) {
   var agent = req.get('user-agent');
   var redirectUrl = 'itms-services://?action=download-manifest&url=https://services.glgresearch.com/go-marketing/Go.plist'; 
   if ( agent.match(/iPhone/) ) {
-    //return res.redirect('itms-services://?action=download-manifest&url=https://services.glgresearch.com/go-marketing/Go.plist');
     res.json(200, {redirect: redirectUrl});
   } else {
-    // Android is used or desktop.  Display notification or error dialog.
-    //res.send(200, 'Unsupported mobile platform');
     res.json(501, {
       unsupported: {
         title: 'Unsupported Mobile Device',
--- src/site/scripts/request.js
+++ src/site/scripts/request.js
@@ -1,7 +1,10 @@
-var request = {
-  init: function() {
+var request = require('browser-request');
+var lo = require('lodash');
 
-    // For request token page, cell number should be the focus input.
+document.addEventListener("DOMContentLoaded", function(e) {
+  e.preventDefault();
+  console.log("DOM fully loaded and parsed");
+  // For request token page, cell number should be the focus input.
     document.querySelector('#cell').focus();
 
     // Setup on-click event listener for App Download Button
@@ -18,13 +21,13 @@ var request = {
     $successOverlay.addEventListener('click', function(e) {
       this.classList.remove('show');
       $successDialog.classList.remove('show');
+      document.querySelector('#cell').focus();
     });
 
     $tokenBtn.addEventListener('click', function(e) {
       $successOverlay.classList.remove('show');
       $successDialog.classList.remove('show');
+      document.querySelector('#cell').focus();
     });
+});
 
-
-  } 
-}
